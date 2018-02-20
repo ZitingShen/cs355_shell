81a03b76b10cc5ba67d21c1fe2ffca4bec5bf3dd
Ziting Shen <zshen@brynmawr.edu>
2018-02-20 16:30:43 -0500

    weird things happen

--- signal.h
+++ signal.h
@@ -8,19 +8,19 @@ process is.
 	    status and update the job status in joblist accordingly.*/
 void SIGCHLD_handler(int sig, siginfo_t *sip, void *notused); 
 
-/* SIGCHLD_handler will check the calling process pid from sip, depending on who the calling
+/* SIGINT_handler will check the calling process pid from sip, depending on who the calling
 process is.
-	(1)if sig received in the main process, do nothing;
- 	(2)if not in the main process, will exit the foreground process in the joblist, update joblist;
+	(1) if there is a foregrounded process, it will exit the foreground process in the joblist, 
+	    update the status of the job in the joblist.
+	(2) if there is no foregrounded process, it will do nothing.
  	After (1) or (2), change the global flag, so thhat shell could continue to the next main loop.*/
 void SIGINT_handler(int sig, siginfo_t *sip, void *notused);
 
-/* SIGCHLD_handler will check the calling process pid from sip, depending on who the calling
+/* SIGSTP_handler will check the calling process pid from sip, depending on who the calling
 process is.
-	(1)if sig received in the main process, do nothing;
-	(2)if not in the main process, will find the foreground process in joblist, and suspend
-	   it by sending SIGSTOP. Then update the joblist.
-	After (1) or (2), change the global flag, so thhat shell could continue to the next main loop*/
+	(1) if there is a foregrounded process, it will find the foreground process in joblist, 
+	    and suspend it by sending SIGSTOP, then update the status of the job in the joblist.
+	(2) if there is no foregrounded process, it will do nothing.*/
 void SIGSTP_handler(int sig, siginfo_t *sip, void *notused);
 
 
