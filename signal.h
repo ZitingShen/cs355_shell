c2faa13609bbe17dc96d41a3d897f579b9eb0852
linyiii <chenlinyi1996@yahoo.com>
2018-02-24 23:37:39 -0500

    edited signal.h and signal.cpp

--- signal.h
+++ signal.h
@@ -1,5 +1,4 @@
-#include<signal.h>
-#include<sys/wait.h>
+#include <csignal>
 
 /* SIGCHLD_handler 
 	Update job status with si_status.
@@ -12,32 +11,4 @@ void SIGCHLD_handler(int sig, siginfo_t *sip, void *notused);
 void SIGINT_handler(int sig, siginfo_t *sip, void *notused);
 
 
-
-void SIGCHLD_handler(int sig, siginfo_t *sip, void *notused){
-	int exit_status = sip -> si_status;
-	pid_t pid = sip -> si_pid;
-	if (WIFEXITED(exit_status)){
-		if (joblist.find_pid(pid) -> status == BG){
-			joblist.find_pid(pid) -> status == DN;
-		}
-		else{
-			//joblit.find_pid(pid) -> status == ;???
-		}
-	}
-	else if (WIFSIGNALED(exit_status)){
-		joblist.find_pid(pid) -> status == TN;
-	}
-	else if (WIFSTOPPED(exit_status)){
-		joblist.find_pid(pid) -> status == ST;
-		cout << "[" << joblist.pid2jid(pid) << "]" << "] (" << pid << ")\tStopped\t\tSignal " << WSTOPSIG(exit_status
-			) << endl;
-	}
-}
-
-void SIGINT_handler(int sig, siginfo *sip, void *notused){
-	cout << "\n" << endl;
-	//print prompt?
-	rl_forced_update_display();
-	//register again??
-}
 
