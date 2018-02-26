6c8cd057810dc1e69f6a46e23b6431e68554726c
linyiii <chenlinyi1996@yahoo.com>
2018-02-25 21:38:05 -0500

    still edit evaluate.cpp

--- evaluate.cpp
+++ evaluate.cpp
@@ -8,44 +8,63 @@ extern struct termios shell_tmodes;
 
 using namespace std;
 
+void no_pipe_exec (string *command, vector<string> command_segment, enum job_status bg_fg){
+	pid_t pid;
+	sigset_t signalSet;  
+  	sigemptyset(&signalSet);
+  	sigaddset(&signalSet, SIGCHLD);
+
+  	/* Block SIGCHLD signal while fork(), setpgid and add joblist */  
+  	sigprocmask(SIG_BLOCK, &signalSet, NULL);
+
+  	/*fork*/
+  	if ((pid = fork()) < ZERO ){
+		cerr << "Failed to fork child process at process " << getpid() << endl;
+	}
+
+	if (pid == ZERO){ //in child process
+		/*setpgid*/
+		if ((setpgid(chld_pid, chld_pid)) < ZERO){
+			ceer << "Failed to set a new group for child process " << pid << "!" << endl;
+		}
+		/*update joblist*/
+		joblist.add(chld_pid, bg_fg, command);
+		sigprocmask(SIG_UNBLOCK, &signalSet, NULL);
+		if (execvp(last_seg.begin(), last_seg) < ZERO){
+			cerr << "Child process of " << getppid() << "failed to execute in bg or the execution is interrupted!" << endl;
+		}
+	}
+	else{ //parent process
+		//do nothing if bg, will clean up in the next loop.
+		if (bg_fg == FG){ //waiting for fg child to complete, need to swap termio, also need to store termio
+			//of child if child is stopeed
+			waitpid(pid, &status, WUNTRACED);
+			/*does this order matter? tcsetpgrp() first or tcgetattr() first?*/
+			tcsetpgrp (shell_terminal, shell_pgid); //bring shell to fg
+			if (WIFSTOPPED(status)){ //store child termio if stopped
+				tcgetattr (shell_terminal, &joblist.find_pid(pid) -> ter); 
+			}
+			tcsetattr (shell_terminal, TCSADRAIN, &shell_tmodes); // restore shell termio
+		}
+	}
+	
+}
+
 void evaluate (string *command, vector<vector<string> > *parsed_segments, bool *cont){
 	string inter_result;
 	int len = parsed_segments -> size();
-	bool bg = FALSE; //whether background
+	//bool bg = FALSE; //whether background
+	enum job_status bg_fg = FG; //default as FG
 	pid_t pid;
+	pid_t chld_pid
 	int status;
 	vector<string> last_seg = parsed_segments -> end();
 	if (len == 1){//no pipe
-		if (last_seg -> end() == "&"){ //check whether background or foreground
-			bg = TRUE;
-		}
-		/*fork a child to execute if not built-in*/
 		if (){//not builtin){}
-			if ((pid = fork()) < ZERO ){
-				cerr << "Failed to fork child process!" << endl;
+			if (last_seg.end() == "&"){ //check whether background or foreground
+				bg_fg = FG;
 			}
-			if (pid == ZERO){ //in child process
-				if (bg){
-					
-				}
-				else{ //if fg, need to swap termio :(
-
-				}
-			}
-			else{ //parent process
-				if (bg){
-					waitpid(pid, &status, WNOHANG | WUNTRACED);
-				}
-				else{ //waiting for fg child to complete, need to swap termio, also need to store termio
-					//of child if child is stopeed
-					waitpid(pid, &status, WUNTRACED);
-					tcsetpgrp (shell_terminal, shell_pgid); //bring shell to fg
-					if (WIFSTOPPED(status)){ //store child termio if stopped
-						tcgetattr (shell_terminal, &joblist.find_pid(pid) -> ter); 
-					}
-					tcsetattr (shell_terminal, TCSADRAIN, &shell_tmodes); // restore shell termio
-				}
-			}
+			no_pipe_exec(*command, last_seg, bg_fg)
 		}
 	}
 	return;
