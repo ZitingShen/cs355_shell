df537c4f31db70e8360d19d355175aa93e3c10ab
linyiii <chenlinyi1996@yahoo.com>
2018-02-27 00:43:42 -0500

    fixing things

--- evaluate.cpp
+++ evaluate.cpp
@@ -94,7 +94,7 @@ void no_pipe_exec (string *command, vector<string> argv, enum job_status bg_fg){
 
 		if (bg_fg == FG){
 			tcsetpgrp (shell_terminal, pid); //bring job to fg
-			tcgetattr (shell_terminal, &shell_tmodes);
+			tcsetattr (shell_terminal, &shell_tmodes);
 		}
 
 		if (execvp(argvc[0], argvc) < 0){
