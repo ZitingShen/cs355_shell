d353b3fd0f459a7816013a257040598a0d3a0dc6
linyiii <chenlinyi1996@yahoo.com>
2018-02-27 00:49:04 -0500

    fixing things

--- evaluate.cpp
+++ evaluate.cpp
@@ -98,8 +98,11 @@ void no_pipe_exec (string *command, vector<string> argv, enum job_status bg_fg){
 			tcsetpgrp (shell_terminal, pid); //bring job to fg
 			tcsetattr (shell_terminal, &shell_tmodes);
 			if (getpgid()==getpgid){
-			cout<<"set new group!"<<endl;
-		}
+				cout<<"set new group!"<<endl;
+			}
+			else{
+				cout<<"still in the same group"<<endl;
+			}
 		}
 
 		if (execvp(argvc[0], argvc) < 0){
